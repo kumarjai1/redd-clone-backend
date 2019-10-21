282d7e147f400b0c963d067cbd851e4dae33edb1
kumarjai1 <jaiessarani@gmail.com>
2019-10-21 15:30:59 -0400

    config and auth file setup

---
+++ src/main/java/com/ga/config/JwtUtil.java
@@ -0,0 +1,72 @@
+package com.ga.config;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JwtUtil implements Serializable{
+	
+	private static final long JWT_TOKEN_VALIDITY = 24 * 60 * 60;
+	
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+		return doGenerateToken(claims, userDetails.getUsername());
+	}
+
+	//this generates the token 
+	private String doGenerateToken(Map<String, Object> claims, String username) {
+		return Jwts.builder().setClaims(claims)
+				.setSubject(username)
+				.setIssuedAt(new Date (System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+				.signWith(SignatureAlgorithm.ES512, secret).compact();
+	}
+	
+	//this returns username from the token
+	public String getUsernameFromToken(String token) {
+		return getClaimFromToken (token, Claims::getSubject);
+	}
+
+	//it gets a single claim
+	private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = getAllClaimsFromToken(token);
+		// TODO Auto-generated method stub
+		return claimsResolver.apply(claims);
+	}
+
+	//this returns the claims object
+	private Claims getAllClaimsFromToken(String token) {
+		return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+	}
+	
+	//checking if token is expired or not and if username is same as token 
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = getUsernameFromToken(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+
+	//checks for token expiration
+	private Boolean isTokenExpired(String token) {
+		final Date expiration = getExpirationDateFromToken(token);
+		return expiration.before(new Date());
+	}
+
+	private Date getExpirationDateFromToken(String token) {
+		// TODO Auto-generated method stub
+		return getClaimFromToken(token, Claims::getExpiration);
+	}
+	
+	
+}
