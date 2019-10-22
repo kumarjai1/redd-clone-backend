1c9fc0107032b2aedd661b330c9b07194493b395
zhu-qin <qqinzhu@gmail.com>
2019-10-22 12:01:05 -0400

    started posts

--- src/main/java/com/ga/entity/Post.java
+++ src/main/java/com/ga/entity/Post.java
@@ -1,5 +1,18 @@
 package com.ga.entity;
 
-public class Post {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "posts")
+public class Post {
+	@Id
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+    
+    @Column(unique = true, nullable = false)
+    private String title;
+	
+    @Column
+    private String description;
 }
