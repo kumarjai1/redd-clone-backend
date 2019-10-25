6d9b4efd67bf7be6b985d65b0e93ef0c80088c90
zhu-qin <qqinzhu@gmail.com>
2019-10-25 15:48:13 -0400

    added tests getUserbyUsername listPost and listComments from user dao

--- src/test/java/com/ga/dao/UserDaoTest.java
+++ src/test/java/com/ga/dao/UserDaoTest.java
@@ -8,6 +8,8 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import com.ga.entity.Comment;
+import com.ga.entity.Post;
 import com.ga.entity.User;
 import com.ga.exception.EntityNotFoundException;
 
@@ -15,6 +17,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -77,6 +82,44 @@ public class UserDaoTest {
         assertNotNull("Test returned null object, expected non-null", savedUser);
         assertEquals(savedUser, user);
     }	
+     
+    @Test
+    public void getUserByUsername_User_Success() {
+    	when(session.createQuery(anyString())).thenReturn(query);
+    	when(query.uniqueResult()).thenReturn(user);
+    	
+    	User fetchedUser = userDao.getUserByUsername("user1");
+    	
+    	assertEquals(fetchedUser, user);
+    }
     
+    @Test
+    public void listPosts_Posts_Success(){
+      	List<Post> testPosts = new ArrayList();
+    	Post post = new Post();
+    	post.setPostId(1L);
+    	post.setTitle("test");
+    	post.setDescription("test");
+    	testPosts.add(post);
+    	user.setPosts(testPosts);
+
+    	List<Post> fetchedPosts = userDao.listPosts(user);
+    	
+    	assertEquals(fetchedPosts, testPosts);
+    }
+    
+    @Test
+    public void listComments_Commentss_Success(){
+    	List<Comment> testComments = new ArrayList();
+    	Comment comment = new Comment();
+    	comment.setCommentId(1L);
+    	comment.setText("test");
+    	testComments.add(comment);
+    	user.setComments(testComments);
+    	    	
+    	List<Comment> fetchedComments = userDao.listComments(user);
+    	
+    	assertEquals(fetchedComments, testComments);
+    }
     
 }
