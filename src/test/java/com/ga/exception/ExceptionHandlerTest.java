8d41f43fa228d1f1c24dad7ca728344ca3af1a03
kumarjai1 <jaiessarani@gmail.com>
2019-10-28 00:05:00 -0400

    added not valid argument exception test

--- src/test/java/com/ga/exception/ExceptionHandlerTest.java
+++ src/test/java/com/ga/exception/ExceptionHandlerTest.java
@@ -6,14 +6,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.request.WebRequest;
 
 import com.ga.dao.UserDao;
 import com.ga.entity.User;
 import com.ga.service.UserServiceImpl;
 
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 
@@ -23,23 +29,26 @@ public class ExceptionHandlerTest {
     public MockitoRule rule = MockitoJUnit.rule();
 	
 	@InjectMocks
-	private UserServiceImpl userService;
-	
-	@InjectMocks
 	private ExceptionHandler exceptionHandler;
 	
 	@Mock
-	private UserDao userDao;
+	private HttpHeaders headers;
+
 	
-	@InjectMocks
-	private User user;
+	@Mock
+	private WebRequest request;
+	
+	@Mock
+	private MethodArgumentNotValidException methodArgumentNotValidException;
+	
 	
 	@Test
 	public void handleException_Exception_Test() {
 		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, "test");
-		ResponseEntity testResponse = new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+		
+		ResponseEntity<ErrorResponse> testResponse = new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	
-		ResponseEntity result = exceptionHandler.handleException(new Exception("test"));
+		ResponseEntity<ErrorResponse> result = exceptionHandler.handleException(new Exception("test"));
 		assertEquals(testResponse.getStatusCode(), result.getStatusCode());
 	}
 	
@@ -50,5 +59,21 @@ public class ExceptionHandlerTest {
 	
 		ResponseEntity result = exceptionHandler.handleEntityNotFoundException(new Exception("test"));
 		assertEquals(testResponse.getStatusCode(), result.getStatusCode());
+	}
+	
+	@Test
+	public void handleMethodArgumentNotValid_ReturnsResponseEntityObject_Test() {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("status", "bad request");
+		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, "Bad Request");
+		ResponseEntity<Object> errorResponseEntity = new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+		
+		ResponseEntity<Object>  testResponse = exceptionHandler.handleMethodArgumentNotValid(methodArgumentNotValidException, 
+				headers, HttpStatus.BAD_REQUEST, request);
+		
+		System.out.println(testResponse.getStatusCode());
+		assertNotNull(testResponse.getStatusCode());
+		assertEquals(testResponse.getStatusCode(), errorResponseEntity.getStatusCode());
 	}
 }
--- src/test/java/com/ga/exception/LoginExceptionTest.java
+++ src/test/java/com/ga/exception/LoginExceptionTest.java
@@ -1,5 +1,7 @@
 package com.ga.exception;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -11,10 +13,7 @@ import com.ga.dao.UserDao;
 import com.ga.entity.User;
 import com.ga.service.UserServiceImpl;
 
-import static org.mockito.Mockito.when;
-
-
-public class EntityNotFoundExceptionTest {
+public class LoginExceptionTest {
 	
 	@Rule
     public MockitoRule rule = MockitoJUnit.rule();
@@ -28,12 +27,11 @@ public class EntityNotFoundExceptionTest {
 	@Mock
 	private UserDao userDao;
 	
-	@Mock
+	@InjectMocks
 	private User user;
 	
-	@org.springframework.web.bind.annotation.ExceptionHandler(EntityNotFoundException.class)
-	@Test(expected=EntityNotFoundException.class)
-	public void loginException_Test() throws LoginException, EntityNotFoundException{
+	@Test(expected = LoginException.class)
+	public void loginException_Test() throws LoginException, EntityNotFoundException {
 		when(userDao.login(user)).thenThrow(new RuntimeException());
 		User user = new User();
 		userService.login(user);
